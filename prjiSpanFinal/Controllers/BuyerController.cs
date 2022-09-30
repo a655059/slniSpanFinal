10ca6cbb8bd1d8d6d20c3a52629e5ae63a73b326
ShawnShen <a655059@hotmail.com.tw>
2022-09-30 10:51:07 +0800

    123

--- prjiSpanFinal/Controllers/BuyerController.cs
+++ prjiSpanFinal/Controllers/BuyerController.cs
@@ -12,5 +12,9 @@ namespace prjiSpanFinal.Controllers
         {
             return View();
         }
+        public IActionResult Order()
+        {
+            return View();
+        }
     }
 }
