8f76ea663023b8b0cbd5eee8cf5e17a377281cca
attact12 <harry80011@gmail.com>
2022-10-03 19:10:41 +0800

    10/03 19:09 優惠券版/按讚好物版

--- prjiSpanFinal/Startup.cs
+++ prjiSpanFinal/Startup.cs
@@ -50,7 +50,7 @@ namespace prjiSpanFinal
             {
                 endpoints.MapControllerRoute(
                     name: "default",
-                    pattern: "{controller=Home}/{action=Index}/{id?}");
+                    pattern: "{controller=Member}/{action=List}/{id?}");
             });
         }
     }
