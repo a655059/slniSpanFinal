c058ad48844234c51b0b4e4696a2c95048f766ae
紹瑋 藍 <maimaisatt@gmail.com>
2022-10-05 16:52:59 +0800

    20221005_cart checked

---
+++ prjiSpanFinal/wwwroot/js/DeliveryShowCart.js
@@ -0,0 +1,12 @@
+﻿$(".selectAll").change(function () {
+    if ($(this).prop("checked")) {
+        $(this).closest("div").siblings().find(".selectItem").prop("checked", true);
+    }
+    else {
+        $(this).closest("div").siblings().find(".selectItem").prop("checked", false);
+    }
+});
+
+function() {
+
+}
