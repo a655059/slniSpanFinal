89bd1708c6ed36bc215bcb951d28829ad49c0b43
ShawnShen <a655059@hotmail.com.tw>
2022-10-06 14:48:43 +0800

    adddd

--- prjiSpanFinal/wwwroot/js/ItemIndex.js
+++ prjiSpanFinal/wwwroot/js/ItemIndex.js
@@ -5,14 +5,13 @@
 });
 
 $(".purchaseStyle").mouseenter(function () {
-    $(this).removeClass("btn-outline-secondary").addClass("btn-outline-warning").closest("div").siblings().children("label").removeClass("btn-outline-warning").addClass("btn-outline-secondary");
     let imgSrc = $(this).siblings(".itemStyleImg").attr("src");
     $(".bigPhoto img").attr("src", imgSrc);
 });
 $(".purchaseStyle").click(function () {
     let price = $(this).siblings(".price").html();
     $(".itemPrice").html(price);
-    $(this).removeClass("btn-outline-warning").removeClass("btn-outline-secondary").addClass("btn-outline-danger").closest("div").siblings().children("label").removeClass("btn-outline-danger").addClass("btn-outline-secondary");
+    $(this).removeClass("btn-outline-secondary").addClass("btn-danger").closest("div").siblings().children("label").removeClass("btn-danger").addClass("btn-outline-secondary");
     let qty = $(this).siblings(".qty").html();
     $(".itemRemainingQty").html("還剩" + qty + "件");
     $("input[name='purchaseCount']").attr("max", qty)
