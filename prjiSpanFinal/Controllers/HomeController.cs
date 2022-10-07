841b45eadb0beb16823938d34de9c739fcc87468
昱騏 胡 <hutoby7@gmail.com>
2022-10-07 19:52:05 +0800

    做了側邊攔和獨立layout

--- prjiSpanFinal/Controllers/HomeController.cs
+++ prjiSpanFinal/Controllers/HomeController.cs
@@ -56,5 +56,10 @@ namespace prjiSpanFinal.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+        public IActionResult SideMenu()
+        {
+            return PartialView();
+        }
+
     }
 }
