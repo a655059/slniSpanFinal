0696f633c097f72c6bc88dba5eeacaec9f4626a7
昱騏 胡 <hutoby7@gmail.com>
2022-10-09 21:58:46 +0800

    設定了powerBi的尺寸，儀錶板內容還在製作中

--- prjiSpanFinal/Models/CMemberHu.cs
+++ prjiSpanFinal/Models/CMemberHu.cs
@@ -2,12 +2,13 @@
 {
     public class CMemberHu
     {
-        public int Id { get; set; } 
-        public string MemName { get; set; } 
-        public string Phone { get; set; } 
+        public int Id { get; set; }
+        public string MemName { get; set; }
+        public string Phone { get; set; }
         public string Address { get; set; }
-        public string Email { get; set; }    
+        public string Email { get; set; }
         public string Password { get; set; }
         public string MemberStatus { get; set; }
+        public string Gender { get; set; }
     }
 }
