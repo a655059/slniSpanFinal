3c7bbad12677f98cf185b5ad0cc3fad8dce63b17
仕旻 張 <michael19950629@gmail.com>
2022-10-11 04:45:19 +0800

    JOB OVER !

--- prjiSpanFinal/wwwroot/js/Layout/layout.js
+++ prjiSpanFinal/wwwroot/js/Layout/layout.js
@@ -1,15 +1,56 @@
 ﻿$(function () {
-    $('#BackTop').click(function () {
-        $('html,body').animate({ scrollTop: 0 }, 333);
-    });
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
-            $('#BackTop').fadeIn(222);
+            $('#BackTop').fadeIn(400);
         } else {
-            $('#BackTop').stop().fadeOut(222);
+            $('#BackTop').fadeOut(400);
         }
-    }).scroll();
+    });
+    $('#BackTop').click(function () {
+        $('html,body').animate({ scrollTop: 0 }, 333);
+        //window.scrollTo({ top: 0, behavior: 'smooth' });
+    });
 });
+
+
+//$(".notification").hover(
+//    function () {
+//        $('#collapseExample1').collapse('show');
+//    }, function () {
+//        $('#collapseExample1').collapse('hide');
+//    }
+//);
+
+//let cost = 0;
+
+//$(`#notification`).mouseenter(function () {
+//     cost++;
+//     checkcollapes();
+// });
+//$(`#collapseExample1`).mouseenter(function () {
+//    cost++;
+//    checkcollapes();
+//});
+//$(`#notification`).mouseleave(function () {
+//        cost--;
+//    checkcollapes();
+//    });
+//$(`#collapseExample1`).mouseleave(function () {
+//    cost--;
+//    checkcollapes();
+//});
+
+//function checkcollapes() {
+//    if (cost <= 0) {
+//        $(`#collapseExample1`).hide();
+//    }
+//    else if (cost > 0) {
+//        $(`#collapseExample1`).show();
+//    }
+//    console.log(cost);
+//}
+
+
 
 const h1 = $("#header1");
 const h2 = document.getElementById("header2");
