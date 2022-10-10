8ba7298471d72548a6cf5feeae4b96a87b4c71d0
昱騏 胡 <hutoby7@gmail.com>
2022-10-11 00:49:11 +0800

    做了檢舉系統的Modal，後台檢舉系統

--- prjiSpanFinal/Models/CReportHu.cs
+++ prjiSpanFinal/Models/CReportHu.cs
@@ -1,12 +1,12 @@
 ﻿namespace prjiSpanFinal.Models
 {
-    public class CReportHu
+    public  class CReportHu
     {
        public virtual CMemberHu CMemberHu { get; set; }
         public int ReportId { get; set; }
         public int ReporterID { get; set; }
        public virtual CProductHu CProductHu { get; set; }
-        public int ProductID { get; set; }
+        public string ProductName { get; set; }
         public string ReportType { get; set; }
         public string Reason { get; set; }
         public string ReportStatus { get; set; }
