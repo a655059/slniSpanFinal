ab4bc6c62526b832cdf1a777748ce60fbac4d29f
昱騏 胡 <hutoby7@gmail.com>
2022-10-10 19:54:04 +0800

    吃完飯回來忘記我做了什麼，但還是推上去一下

---
+++ prjiSpanFinal/Models/CReportHu.cs
@@ -0,0 +1,12 @@
+﻿namespace prjiSpanFinal.Models
+{
+    public class CReportHu
+    {
+        public int ReportId { get; set; }
+        public int ReporterID { get; set; }
+        public int ProductID { get; set; }
+        public string ReportType { get; set; }
+        public string Reason { get; set; }
+        public string ReportStatus { get; set; }
+    }
+}
