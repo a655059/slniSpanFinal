01697ab80879fd4952d4b7db9e214229a510634e
昱騏 胡 <hutoby7@gmail.com>
2022-10-12 18:06:06 +0800

    改一些程式排版類的枝微末節，對程式跑起來完全沒有影響

--- prjiSpanFinal/Controllers/ManagementController.cs
+++ prjiSpanFinal/Controllers/ManagementController.cs
@@ -25,7 +25,7 @@ namespace prjiSpanFinal.Controllers
             }
 
         }
-        #region
+        #region DATASRegion
         public void ADDDATAS()
         {
             for (int i = 0; i <= 20; i++)
@@ -93,7 +93,7 @@ namespace prjiSpanFinal.Controllers
             Reports.Add(cReport);
         }
         #endregion
-        #region
+        #region ReportRegion
         public IActionResult ReportList()
         {
             return View(Reports);
@@ -128,13 +128,13 @@ namespace prjiSpanFinal.Controllers
             return RedirectToAction("ReportList");
         }
         #endregion
-        #region
+        #region PowerBiRegion
         public IActionResult PowerBi()
         {
             return View();
         }
         #endregion
-        #region
+        #region ProductRegion
         public IActionResult ProductList()
         {
             return View(Products);
@@ -163,9 +163,6 @@ namespace prjiSpanFinal.Controllers
             }
             return RedirectToAction("ProductList");
         }
-
-
-
         public IActionResult ProductDelete(int? id)
         {
             if (id != null)
@@ -201,7 +198,7 @@ namespace prjiSpanFinal.Controllers
             return RedirectToAction("ProductList");
         }
         #endregion
-        #region
+        #region ProductDetailRegion
         public IActionResult ProductDetailList(int? id)
         {
             var product = from i in ProductDetails
@@ -240,7 +237,7 @@ namespace prjiSpanFinal.Controllers
             return RedirectToAction("ProductList");
         }
         #endregion
-        #region
+        #region MemberRegion
         public IActionResult MemberList()
         {
             return View(members);
@@ -325,7 +322,7 @@ namespace prjiSpanFinal.Controllers
             return RedirectToAction("MemberList");
         }
         #endregion
-        #region
+        #region OrderRegion
         public IActionResult OrderList()
         {
             return View(Orders);
