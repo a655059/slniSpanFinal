3d158a35a48d5bb48db780aea88045310137f999
紹瑋 藍 <maimaisatt@gmail.com>
2022-10-13 00:22:24 +0800

    20221012_OrderSuccess Page Start

--- prjiSpanFinal/Controllers/DeliveryController.cs
+++ prjiSpanFinal/Controllers/DeliveryController.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using MimeKit;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net.Mail;
 using System.Threading.Tasks;
 
 namespace prjiSpanFinal.Controllers
@@ -20,15 +22,28 @@ namespace prjiSpanFinal.Controllers
         {
             return View();
         }
-        public IActionResult CheckoutPartial(int id)
-        {
-            return PartialView("~/Views/Delivery/_DeliveryCheckoutPartial.cshtml", id);
-        }
         public IActionResult AddComment()
         {
             return View();
         }
-        
-
+        public IActionResult CheckoutForm(int? count)
+        {
+            return ViewComponent("DeliveryFillCheckoutForm", count);
+        }
+        public IActionResult OrderSuccess()
+        {
+            MimeMessage message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Jacob", "maimaisatt@gmail.com"));
+            message.To.Add(new MailboxAddress("Jacob", "maimaisatt@gmail.com"));
+            message.Subject = "測試一下";
+            BodyBuilder builder = new BodyBuilder();
+            builder.TextBody = "你好，這是測試";
+            message.Body = builder.ToMessageBody();
+            using(SmtpClient client = new SmtpClient())
+            {
+                
+            }
+            return View();
+        }
     }
 }
