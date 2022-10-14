3b6a2668681442793615292760f3c4b24b01c2f4
紹瑋 藍 <maimaisatt@gmail.com>
2022-10-14 15:05:44 +0800

    ggg

---
+++ prjiSpanFinal/ViewComponents/ProductCardViewComponent.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
+
+namespace prjiSpanFinal.ViewComponents
+{
+    public class ProductCardViewComponent : ViewComponent
+    {
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            return View();
+        }
+    }
+}
