f656325ed7b9364a8e76702b1d16f00e7939c4b4
昱騏 胡 <hutoby7@gmail.com>
2022-10-14 13:14:16 +0800

    在newManagement控制器做了newProductList可以分頁

---
+++ prjiSpanFinal/Controllers/newManagementController.cs
@@ -0,0 +1,46 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcPaging;
+using prjiSpanFinal.Models;
+using prjiSpanFinal.ViewModel;
+using System.Linq;
+
+
+namespace prjiSpanFinal.Controllers
+{
+    public class newManagementController : Controller
+    {
+        private readonly iSpanProjectContext db;
+        public newManagementController()
+        {
+            db = new iSpanProjectContext();
+        }
+        #region ProductRegion
+        public IActionResult ProductList()
+        {
+            var Q = from i in db.Products 
+                            select i;
+            return View();
+        }
+        public IActionResult newProductList()
+        {
+
+            var C=new CProductListViewModel();
+            var Q = from i in db.Products
+                    select i;
+            C.Products = Q.ToPagedList(C.Page > 0 ? C.Page - 1 : 0, PageSize); ;
+            int ID=0;
+            C.id = ID;
+            return PartialView(C);
+        }
+        public IActionResult ProductEdit(int? id)
+        {
+            if (id != null)
+            {
+                var Q = db.Products.FirstOrDefault(i => i.ProductId == id);
+                return View(Q);
+            }
+            return RedirectToAction("ProductList");
+        }
+        #endregion
+    }
+}
