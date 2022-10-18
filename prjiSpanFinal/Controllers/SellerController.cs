8acb25bb31493b2c927e05c55f800e57b260d8a7
芳季 游 <wijgm38@gmail.com>
2022-10-18 17:25:48 +0800

    1018

--- prjiSpanFinal/Controllers/SellerController.cs
+++ prjiSpanFinal/Controllers/SellerController.cs
@@ -69,24 +69,56 @@ namespace prjiSpanFinal.Controllers
             return View();
         }
 
-        public IActionResult NewIndex()
+        public IActionResult NewIndex(int id)
         {
-            //var q = from c in _db.Products where c.MemberId == 
-            //var productID = _db.Products.Select(n => n.ProductId);
-            //var productName = _db.Products.Where(n => n.ProductId = productID).Select(n => n.);
-            //var Quantity = _db.ProductDetails.Select(n => n.Quantity).ToList();
-            //var UnitPrice = _db.ProductDetails.Select(n => n.UnitPrice).ToList();
-            //var Pic = _db.ProductDetails.Select(n => n.Pic).ToList();
+            id = 2;//先把ID寫死
+            var myproductlist = _db.Products.Where(n => n.MemberId == id).Select(n => n.ProductId).ToList(); //賣家所有商品ID
+            var q1 = _db.Products.Where(n => n.MemberId == id).Select(n => n).ToList();//賣家所有商品
+            var q2 = _db.ProductDetails.Where(n => myproductlist.Contains(n.ProductId)).Select(n => n).ToList(); //Contains是只把賣家所有商品ID全部挑出來
+            List<string> listName = new List<string>();
+
+            List<List<string>> listStyle = new List<List<string>>();
+            //  List<一個商品有兩個Style>  =   <一個商品有兩個Style>  <一個商品有兩個Style>  <一個商品有兩個Style>
+
+            List<List<int>> listQty = new List<List<int>>();
+            List<List<decimal>> listPrice = new List<List<decimal>>();
+            List<List<byte[]>> listPic = new List<List<byte[]>>();
+            
+            for (int i = 0; i < myproductlist.Count; i++)  //外迴圈把所有商品列出來存進List
+            {
+                List<string> sublistStyle = new List<string>();
+                List<int> sublistQty = new List<int>();
+                List<decimal> sublistPrice = new List<decimal>();
+                List<byte[]> sublistPic = new List<byte[]>();
+
+                listName.Add(q1[i].ProductName);//把商品名稱存進去
+                var detail = q2.Where(p => p.ProductId == q1[i].ProductId).ToList();//找出所有同ID商品的資料
+
+                for(int j = 0;j<detail.Count;j++)//內迴圈把同ID商品所有Style和相關資料列出來存進List
+                {
+                    sublistStyle.Add(detail[j].Style);
+                    sublistQty.Add(detail[j].Quantity);
+                    sublistPrice.Add(detail[j].UnitPrice);
+                    sublistPic.Add(detail[j].Pic);
+                }
+                listQty.Add(sublistQty);  //把所有商品的數量存進去
+                listPrice.Add(sublistPrice);
+                listPic.Add(sublistPic);
+                listStyle.Add(sublistStyle);
+            }
+
             CSellerNewIndexToViewViewModel x = new CSellerNewIndexToViewViewModel
             {
-                //productName = productName,
-                //Style = Style.ToString(),
-                //Quantity = Convert.ToInt32(Quantity),
-                //UnitPrice = Convert.ToDecimal(UnitPrice),
-                //Pic= Pic
+                productName = listName,
+                Style = listStyle,
+                Quantity = listQty,
+                UnitPrice = listPrice,
+                Pic = listPic
             };
+
             return View(x);
         }
+
 
         public IActionResult Coupon()
         {
