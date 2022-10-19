c84892ae6a8ccac1d508481fd60f4aa6d349baf6
a655059 <46038509+a655059@users.noreply.github.com>
2022-10-19 20:37:01 +0800

    123

--- prjiSpanFinal/Startup.cs
+++ prjiSpanFinal/Startup.cs
@@ -33,8 +33,6 @@ namespace prjiSpanFinal
             services.AddControllersWithViews();
             services.AddSession();
             services.AddSignalR();
-            services.AddMvc();
-            services.AddHttpContextAccessor();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
