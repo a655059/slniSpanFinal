6f43ffe75e3aadec9f6ffdaf04a6f09204278ffb
ShawnShen <a655059@hotmail.com.tw>
2022-10-19 09:10:30 +0800

    123

--- prjiSpanFinal/Startup.cs
+++ prjiSpanFinal/Startup.cs
@@ -31,6 +31,7 @@ namespace prjiSpanFinal
             });
             services.AddControllersWithViews();
             services.AddSession();
+            services.AddSignalR();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
