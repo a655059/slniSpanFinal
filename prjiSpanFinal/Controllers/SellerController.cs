0b5be2d96c0b377c14ce2413f8468190c7037de2
ShawnShen <a655059@hotmail.com.tw>
2022-10-21 09:14:12 +0800

    123123

--- prjiSpanFinal/Controllers/SellerController.cs
+++ prjiSpanFinal/Controllers/SellerController.cs
@@ -85,7 +85,7 @@ namespace prjiSpanFinal.Controllers
         [HttpPost]
         public IActionResult Create(string json, CSellerViewToCreateViewModel VM)
         {
-            return 
+            return View();
         }
 
 
@@ -104,7 +104,42 @@ namespace prjiSpanFinal.Controllers
             {
                 return RedirectToAction("Login", "Member");
             }
-            return View(id);
+            iSpanProjectContext dbcontext = new iSpanProjectContext();
+            return View(dbcontext.Orders.Where(o => o.OrderId == id).Select(o => new OrderDetailViewModel()
+            {
+                OrderId = o.OrderId,
+                SellerId = o.OrderDetails.FirstOrDefault().ProductDetail.Product.MemberId,
+                SellerAcc = o.OrderDetails.FirstOrDefault().ProductDetail.Product.Member.MemberAcc,
+                BuyerId = o.MemberId,
+                BuyerAcc = o.Member.MemberAcc,
+                OrderDatetime = o.OrderDatetime,
+                RecieveAdr = o.RecieveAdr,
+                FinishDate = o.FinishDate,
+                CouponName = o.Coupon.CouponName,
+                Discount = o.Coupon.Discount,
+                IsFreeDelivery = o.Coupon.IsFreeDelivery,
+                OrderStatusName = o.Status.OrderStatusName,
+                ShipperStatusId = o.StatusId,
+                ShipperName = o.Shipper.ShipperName,
+                ShipperFee = o.Shipper.Fee,
+                ShipperPhone = o.Shipper.Phone,
+                PaymentDate = o.PaymentDate,
+                ShippingDate = o.ShippingDate,
+                ReceiveDate = o.ReceiveDate,
+                PaymentName = o.Payment.PaymentName,
+                PaymentFee = o.Payment.Fee,
+                OrderMessage = o.OrderMessage,
+                OrderDetailId = o.OrderDetails.Select(o => o.OrderDetailId).ToList(),
+                ProductDetailId = o.OrderDetails.Select(o => o.ProductDetailId).ToList(),
+                Quantity = o.OrderDetails.Select(o => o.Quantity).ToList(),
+                OrderDetailReceiveDate = o.OrderDetails.Select(o => o.ReceiveDate).ToList(),
+                ShipStatusName = o.OrderDetails.Select(o => o.ShippingStatus.ShipStatusName).ToList(),
+                Unitprice = o.OrderDetails.Select(o => o.Unitprice).ToList(),
+                ProductId = o.OrderDetails.FirstOrDefault().ProductDetail.ProductId,
+                Style = o.OrderDetails.Select(o => o.ProductDetail.Style).ToList(),
+                Pic = o.OrderDetails.Select(o => o.ProductDetail.Pic).ToList(),
+                ProductName = o.OrderDetails.Select(o => o.ProductDetail.Product.ProductName).ToList(),
+            }).ToList());
         }
 
 
