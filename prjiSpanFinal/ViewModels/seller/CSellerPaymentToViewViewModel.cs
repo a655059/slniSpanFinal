4dd4da7daed06cfe1ee32b242da6c5d72e28b269
芳季 游 <wijgm38@gmail.com>
2022-10-24 09:05:49 +0800

    1024

--- prjiSpanFinal/ViewModels/seller/CSellerPaymentToViewViewModel.cs
+++ prjiSpanFinal/ViewModels/seller/CSellerPaymentToViewViewModel.cs
@@ -13,7 +13,17 @@ namespace prjiSpanFinal.ViewModels.seller
         public List<int> ProductID { get; set; }
         public List<int> ShipperId { get; set; }
         public List<string> ShipperName { get; set; }
-
-
+        /// <summary>
+        /// 傳進來的MB有的物流資料
+        /// </summary>
+        public int ShipperIdToShip { get; set; }
+        /// <summary>
+        /// 1=true,0=false
+        /// </summary>
+        public int CheckedOX { get; set; }
+        /// <summary>
+        /// 為了要給畫面客戶儲存的物流
+        /// </summary>
+        public List<CSellerPaymentToViewViewModel>x { get; set; }
     }
 }
