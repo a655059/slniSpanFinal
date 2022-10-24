944c3657adfc766d4df10f4d26c01f8ac0ce2941
ShawnShen <a655059@hotmail.com.tw>
2022-10-24 12:28:49 +0800

    123

--- prjiSpanFinal/wwwroot/js/Layout/layout.js
+++ prjiSpanFinal/wwwroot/js/Layout/layout.js
@@ -1,4 +1,7 @@
-﻿$(function () {
+﻿let mymemid = $("#msgmemid").val();
+let header2exist = $("#header2").length != 0;
+
+$(function () {
     $(window).scroll(function () {
         if ($(this).scrollTop() > 300) {
             $('#BackTop').fadeIn(400);
@@ -10,30 +13,91 @@
         $('html,body').animate({ scrollTop: 0 }, 333);
     });
 });
-const h1 = $("#header1");
-const h2 = document.getElementById("header2");
-sizeble();
-addEventListener('resize', async () =>
-    await sizeble());
-function sizeble() {
-    h2.style.marginTop = (h1.height() + 16) + "px";
+if (header2exist) {
+    const h1 = $("#header1");
+    const h2 = document.getElementById("header2");
+    sizeble();
+    addEventListener('resize', async () =>
+        await sizeble());
+    function sizeble() {
+        h2.style.marginTop = (h1.height() + 16) + "px";
+    }
 }
+
 
 
 //搜尋
 let searchInput = document.querySelector("#SearchInputTxT");
 let searchBtn = document.querySelector("#SearchInputbtn");
 
-searchInput.addEventListener("input", () => {
-    let searchkeyword = searchInput.value;
-    let url = `/Category/SearchResult/?keyword=${searchkeyword}`;
-    searchBtn.setAttribute("href", url);
-})
+if (header2exist) {
+    searchInput.addEventListener("input", () => {
+        let searchkeyword = searchInput.value;
+        let url = `/Category/SearchResult/?keyword=${searchkeyword}`;
+        searchBtn.setAttribute("href", url);
+    })
 
-searchInput.addEventListener("keypress", event => {
-    if (event.key === "Enter") {
-        event.preventDefault();
-        searchBtn.click();
-    }
-})
+    searchInput.addEventListener("keypress", event => {
+        if (event.key === "Enter") {
+            event.preventDefault();
+            searchBtn.click();
+        }
+    })
+}
 
+
+//notification
+function ticktotimestamp(today) {
+    
+    tms = today.getHours().toString().padStart(2, '0') + today.getMinutes().toString().padStart(2, '0') + today.getSeconds().toString().padStart(2, '0') + today.getMilliseconds().toString().padStart(3, '0') + today.getFullYear().toString() + (today.getMonth() + 1).toString().padStart(2, '0') + today.getDate().toString().padStart(2, '0');
+    console.log(tms)
+    return tms;
+}
+
+
+if (mymemid == null) {
+    $(".notification").css("display", "none");
+}
+else {
+    $("#notificationlistbox").html("");
+    $.getJSON("/MsgApi/GetNotificationbyID", { id: mymemid }, function (data) {
+        let hrcount = 0;
+        console.log(data);
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].haveRead == false) {
+                hrcount++;
+            }
+            $("#notificationlistbox").append(`<li class="notification-item-listitem">
+                                        <a href="${data[i].link}" class="d-flex justify-content-between linknoline">
+                                            <div class="d-flex flex-row">
+                                                <div>
+                                                    <img src="data:image;base64,${data[i].iconPic}" alt="avatar" class="d-flex align-self-center me-3" style="width: 45px; height: 45px; border-radius: 50%;">
+                                                    <span class="badge bg-success badge-dot"></span>
+                                                </div>
+                                                <div class="pt-1">
+                                                    <p class="mb-0">${data[i].text}</p>
+                                                </div>
+                                            </div>
+                                            <div class="pt-1">
+                                                <input type="hidden" class="msgbodytimestamp" value="${ticktotimestamp(new Date(data[i].time))}"/>
+                                                <p class="small text-muted mb-1"></p>
+                                            </div>
+                                        </a>
+                                    </li>`);
+        }
+        if (hrcount != 0) {
+            $(".linknoline").append(`<div class="qty notificationqty">${$(".notificationqty").html(hrcount)}</div>`);
+        }
+        else {
+            
+        }
+        
+        refreshtimestamp();
+    });
+
+    $(".notification").click(function () {
+        $.getJSON("/MsgApi/HaveReadAllNoti", { id: mymemid }, function (data) {
+        });
+        $(".notificationqty").html("");
+    })
+}
