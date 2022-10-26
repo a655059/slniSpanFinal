2565e07f9014463db1cf1ca61b2e90a5f5672197
仕旻 張 <michael19950629@gmail.com>
2022-10-26 20:57:01 +0800

    MemberCenterCoupon

---
+++ prjiSpanFinal/ViewModels/Member/CouponFactory.cs
@@ -0,0 +1,18 @@
+﻿using prjiSpanFinal.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjiSpanFinal.ViewModels.Member
+{
+    public class CouponFactory
+    {
+        public List<Coupon> cGetCouponWallet(List<Coupon> listc,int MemID)
+        {
+            iSpanProjectContext _db = new iSpanProjectContext();
+            listc = _db.CouponWallets.Where(c => c.MemberId == MemID).Select(c=>c.Coupon).ToList();
+            return listc;
+        }
+    }
+}
