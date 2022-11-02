54ec8b7bad7a29cf64a8e614373d4238090f2442
紹瑋 藍 <maimaisatt@gmail.com>
2022-11-03 02:25:16 +0800

    20221102_CountdownStart

---
+++ prjiSpanFinal/Hubs/CountdownHub.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace prjiSpanFinal.Hubs
+{
+    public class CountdownHub : Hub
+    {
+        public async Task Countdown(int biddingID)
+        {
+            await Clients.All.SendAsync("", biddingID);
+        }
+    }
+}
