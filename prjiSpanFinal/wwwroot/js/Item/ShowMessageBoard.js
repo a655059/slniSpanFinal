7bf2bc0d2a943f131d7cdb8bb3ec36924da1b224
紹瑋 藍 <maimaisatt@gmail.com>
2022-11-03 02:12:01 +0800

    222

--- prjiSpanFinal/wwwroot/js/Item/ShowMessageBoard.js
+++ prjiSpanFinal/wwwroot/js/Item/ShowMessageBoard.js
@@ -8,7 +8,7 @@
     }
     for (let i = 0; i < maxLayer; i++) {
         $(`.layer${i}`).each(function () {
-            if ($(this).next(".messageBoardID") != null) {
+            if ($(this).next(".messageBoardID").length > 0) {
                 const nextLayer = Number($(this).next(".messageBoardID").attr("class").split(' ')[0].substring(5));
                 if (nextLayer > i) {
                     $(this).find(".messageFooter").append(`<div class="collapseMessage" style="cursor:pointer">收合</div>`);
@@ -23,26 +23,46 @@ $(".messageReplyBtn").click(function () {
 });
 
 $(".messageBoardVC").on("click", ".collapseMessage", function () {
-    const thisIndex = Number($(".messageBox .messageBoardID").index($(this).closest(".messageBoardID")));
-
+    //const thisIndex = Number($(".messageBox .messageBoardID").index($(this).closest(".messageBoardID")));
     const thisLayerNumber = Number($(this).closest(".messageBoardID").attr("class").split(' ')[0].substring(5));
-    let count = 0;
-    $(this).closest(".messageBoardID").nextAll(".messageBoardID").each(function () {
-
-        const layerNumber = Number($(this).attr("class").split(' ')[0].substring(5));
-        if (layerNumber > thisLayerNumber) {
-            count++;
+    if ($(this).closest(".messageBoardID").nextAll(".messageBoardID").length > 0) {
+        if ($(this).html() == "收合") {
+            $(this).closest(".messageBoardID").nextAll(".messageBoardID").each(function () {
+                const layerNumber = Number($(this).attr("class").split(' ')[0].substring(5));
+                if (layerNumber > thisLayerNumber) {
+                    $(this).removeClass("d-none").addClass("d-none");
+                }
+                else {
+                    return false;
+                }
+            })
+            $(this).html("展開");
         }
         else {
-            return false;
+            $(this).closest(".messageBoardID").nextAll(".messageBoardID").each(function () {
+                const layerNumber = Number($(this).attr("class").split(' ')[0].substring(5));
+                if (layerNumber > thisLayerNumber) {
+                    $(this).removeClass("d-none");
+                    if ($(this).has(".collapseMessage").length > 0) {
+                        $(this).find(".collapseMessage").html("收合");
+                    }
+                }
+                else {
+                    return false;
+                }
+            })
+            $(this).html("收合");
         }
-    })
-    if ($(this).html() == "展開") {
-        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).removeClass("d-none");
-        $(this).html("收合");
-        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).find(".collapseMessage").html("收合");
-    } else {
-        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).removeClass("d-none").addClass("d-none");
-        $(this).html("展開");
     }
+    //if (count > 0) {
+    //    if ($(this).html() == "展開") {
+    //        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).removeClass("d-none");
+    //        $(this).html("收合");
+    //        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).find(".collapseMessage").html("收合");
+    //    } else {
+    //        $(".messageBox .messageBoardID").slice(thisIndex + 1, thisIndex + count + 1).removeClass("d-none").addClass("d-none");
+    //        $(this).html("展開");
+    //    }
+    //}
+    
 });
