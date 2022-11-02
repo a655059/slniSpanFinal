97644ea59dc99fc2fd8574479ee2ad872fc44c14
ShawnShen <a655059@hotmail.com.tw>
2022-11-02 14:46:44 +0800

    123

---
+++ prjiSpanFinal/Models/Bidding.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace prjiSpanFinal.Models
+{
+    public partial class Bidding
+    {
+        public Bidding()
+        {
+            AddBiddingCalendars = new HashSet<AddBiddingCalendar>();
+            AutoBiddings = new HashSet<AutoBidding>();
+            BiddingDetails = new HashSet<BiddingDetail>();
+        }
+
+        public int BiddingId { get; set; }
+        public int ProductDetailId { get; set; }
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }
+        public int StartPrice { get; set; }
+
+        public virtual ProductDetail ProductDetail { get; set; }
+        public virtual ICollection<AddBiddingCalendar> AddBiddingCalendars { get; set; }
+        public virtual ICollection<AutoBidding> AutoBiddings { get; set; }
+        public virtual ICollection<BiddingDetail> BiddingDetails { get; set; }
+    }
+}
---
+++ prjiSpanFinal/Models/BiddingDetail.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+
+#nullable disable
+
+namespace prjiSpanFinal.Models
+{
+    public partial class BiddingDetail
+    {
+        public int BiddingDetailId { get; set; }
+        public int BiddingId { get; set; }
+        public int MemberId { get; set; }
+        public int Price { get; set; }
+
+        public virtual Bidding Bidding { get; set; }
+        public virtual MemberAccount Member { get; set; }
+    }
+}
