701e17e497f116bab98fd0d9adf7b5d9d68ac3a7
attact12 <harry80011@gmail.com>
2022-11-04 20:42:54 +0800

    正式會員驗證加上時間限制

--- prjiSpanFinal/ViewModels/CDictionary.cs
+++ prjiSpanFinal/ViewModels/CDictionary.cs
@@ -7,5 +7,6 @@ namespace prjiSpanFinal.ViewModels
         public static readonly string SK_LOGIN_VALIDATION = "SK_LOGIN_VALIDATION";
         public static readonly string SK_PURCHASEITEMINFO = "SK_PURCHASEITEMINFO";
         public static readonly string SK_ALL_INFO_TO_SHOW_CHECKOUT = "SK_ALL_INFO_TO_SHOW_CHECKOUT";
+        public static readonly string SK_MAILCHECK= "SK_MAILCHECK";
     }
 }
