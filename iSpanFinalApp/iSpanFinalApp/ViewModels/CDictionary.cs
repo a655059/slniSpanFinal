ed36a1bb30288972636e69eba78c90e24edba224
a655059 <46038509+a655059@users.noreply.github.com>
2022-11-08 06:43:39 +0800

    Delete iSpanFinalApp directory

--- iSpanFinalApp/iSpanFinalApp/ViewModels/CDictionary.cs
+++
@@ -1,9 +0,0 @@
-﻿
-namespace iSpanFinalApp.ViewModels
-{ 
-    public class CDictionary
-    {
-        public static readonly string SK_LOGINED_USER= "SK_LOGINED_USER";
-        public static readonly string SK_ORDER = "SK_ORDER";
-    }
-}
