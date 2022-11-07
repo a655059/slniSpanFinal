a4c058ea1e947a250508525d5cc7fdde11a177c2
a655059 <46038509+a655059@users.noreply.github.com>
2022-11-07 09:58:11 +0800

    123

---
+++ prjiSpanFinal/ViewModels/App/CComment.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace prjiSpanFinal.Models.App
+{
+    public partial class CComment
+    {
+        public byte[] memPic { get; set; }
+        public int CommentId { get; set; }
+        public int OrderDetailId { get; set; }
+        public string Comment1 { get; set; }
+        public byte CommentStar { get; set; }
+        public DateTime CommentTime { get; set; }
+        public int ShipperStar { get; set; }
+        public string Comment2 { get; set; }
+        public string Comment3 { get; set; }
+
+        public List<byte[]> cpics { get; set; }
+
+        public string commentacc { get; set; }
+    }
+}
---
+++ prjiSpanFinal/ViewModels/App/CProduct.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+
+
+namespace prjiSpanFinal.Models.App
+{
+    public partial class CProduct
+    {
+        public int ProductId { get; set; }
+        public string ProductName { get; set; }
+        public int SmallTypeId { get; set; }
+        public int MemberId { get; set; }
+        public int RegionId { get; set; }
+        public string Description { get; set; }
+        public int ProductStatusId { get; set; }
+        public DateTime EditTime { get; set; }
+        public int CustomizedCategoryId { get; set; }
+        public bool IsFeaturedProduct { get; set; }
+    }
+}
