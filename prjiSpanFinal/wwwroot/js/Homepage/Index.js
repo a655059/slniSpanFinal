eea7a894db5107a0df235d83c8dc4048678600bc
仕旻 張 <michael19950629@gmail.com>
2022-11-13 16:44:51 +0800

    123

--- prjiSpanFinal/wwwroot/js/Homepage/Index.js
+++ prjiSpanFinal/wwwroot/js/Homepage/Index.js
@@ -4,7 +4,7 @@ const moreitembtn = document.querySelector("#moreitembtn");
 const itemcol = document.querySelector("#itemcol");
 let itemcount = document.querySelector('#itemCount').value;
 let count = 0;
-let ruler = 334;
+let ruler = 340;
 let counter = 1;
 async function moreitem() {
     if (count < 5 ) {
