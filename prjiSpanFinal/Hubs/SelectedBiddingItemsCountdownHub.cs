65a284ec84202426681261202260591d11c0e7e7
紹瑋 藍 <maimaisatt@gmail.com>
2022-11-14 17:32:37 +0800

    20221114_start

--- prjiSpanFinal/Hubs/SelectedBiddingItemsCountdownHub.cs
+++ prjiSpanFinal/Hubs/SelectedBiddingItemsCountdownHub.cs
@@ -10,27 +10,27 @@ namespace prjiSpanFinal.Hubs
 {
     public class SelectedBiddingItemsCountdownHub :Hub
     {
-        public async Task SelectedBiddingItemsCountdown(List<int> biddingIDs)
+        public async Task SelectedBiddingItemsCountdown(/*List<int> biddingIDs*/)
         {
-            if (biddingIDs.Count > 0)
-            {
-                iSpanProjectContext dbContext = new iSpanProjectContext();
-                var biddings = dbContext.Biddings.ToList();
-                while (true)
-                {
-                    List<string> remainingTimes = new List<string>();
+            //if (biddingIDs.Count > 0)
+            //{
+            //    iSpanProjectContext dbContext = new iSpanProjectContext();
+            //    var biddings = dbContext.Biddings.ToList();
+            //    while (true)
+            //    {
+            //        List<string> remainingTimes = new List<string>();
 
-                    foreach (var a in biddingIDs)
-                    {
-                        DateTime endTime = biddings.Where(i => i.BiddingId == a).Select(i => i.EndTime).FirstOrDefault();
-                        TimeSpan remainingTime = endTime - DateTime.Now;
-                        string time = remainingTime.Days + "天" + remainingTime.Hours + "時" + remainingTime.Minutes + "分" + remainingTime.Seconds + "秒";
-                        remainingTimes.Add(time);
-                    }
-                    await Clients.All.SendAsync("ShowItemCountdown", remainingTimes, biddingIDs);
-                    Thread.Sleep(1000);
-                }
-            }
+            //        foreach (var a in biddingIDs)
+            //        {
+            //            DateTime endTime = biddings.Where(i => i.BiddingId == a).Select(i => i.EndTime).FirstOrDefault();
+            //            TimeSpan remainingTime = endTime - DateTime.Now;
+            //            string time = remainingTime.Days + "天" + remainingTime.Hours + "時" + remainingTime.Minutes + "分" + remainingTime.Seconds + "秒";
+            //            remainingTimes.Add(time);
+            //        }
+            //        await Clients.All.SendAsync("ShowItemCountdown", remainingTimes, biddingIDs);
+            //        Thread.Sleep(1000);
+            //    }
+            //}
         }
     }
 }
